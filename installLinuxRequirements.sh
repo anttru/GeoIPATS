1232ecc132d3b56efd1de6f4350e9ccf5d189107
Antonio Trujillo <antrusant@gmail.com>
2022-05-28 23:23:55 +0200

    Added Linux Scripts

---
+++ installLinuxRequirements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install python3-pip
+sudo apt-get install python-tk
+sudo apt-get install python3-pil python3-pil.imagetk
+sudo pip install IPy
+sudo pip install tkintermapview
